fd94e682236d41d00e182c0d768d5015d8a6b0c6
abanoubsamir <abanoubmalek74@gmail.com>
2026-03-02 23:38:40 +0200

    update

--- src/protectedRoutes/ProductedAuthRoute.jsx
+++ src/protectedRoutes/ProductedAuthRoute.jsx
@@ -1,7 +1,16 @@
-import React, { useContext } from "react";
+// import React, { useContext } from "react";
 
-export default function ProductedAuthRoute({ children }) {
-  const { userToken } = useContext(authContext);
-  const isLoggedIn = !!userToken;
-  return isLoggedIn ? children : <Navigate to={"/"} />;
+// export default function ProductedAuthRoute({ children }) {
+//   const { userToken } = useContext(authContext);
+//   const isLoggedIn = !!userToken;
+//   return isLoggedIn ? children : <Navigate to={"/"} />;
+// }
+
+import React, { useContext } from "react";
+import { Navigate } from "react-router-dom";
+import { authContext } from "../contexts/authContext";
+
+export default function ProtectedAuthRoute({ children }) {
+  const { user } = useContext(authContext); 
+  return user ? children : <Navigate to="/signup" replace />;
 }
--- src/protectedRoutes/ProductedRoute.jsx
+++ src/protectedRoutes/ProductedRoute.jsx
@@ -1,6 +1,15 @@
-import React, { Children } from 'react'
+// import React, { Children } from 'react'
 
-export default function ProductedRoute({children}) {
-  const isLoggedIn = !!localStorage.getItem("token")
-  return isLoggedIn ? children : <Navigate to= {"/signin"}/>
+// export default function ProductedRoute({children}) {
+//   const isLoggedIn = !!localStorage.getItem("token")
+//   return isLoggedIn ? children : <Navigate to= {"/signin"}/>
+// }
+
+import React, { useContext } from "react";
+import { Navigate } from "react-router-dom";
+import { authContext } from "../contexts/authContext";
+
+export default function ProtectedRoute({ children }) {
+  const { user } = useContext(authContext);
+  return user ? children : <Navigate to="/signup" replace />;
 }
