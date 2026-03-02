d09f19a96eacdb25ff1906a2adf6be922cf831c8
abanoubsamir <abanoubmalek74@gmail.com>
2026-03-02 23:55:25 +0200

    update

--- src/protectedRoutes/ProductedRoute.jsx
+++ src/protectedRoutes/ProductedRoute.jsx
@@ -1,10 +1,3 @@
-// import React, { Children } from 'react'
-
-// export default function ProductedRoute({children}) {
-//   const isLoggedIn = !!localStorage.getItem("token")
-//   return isLoggedIn ? children : <Navigate to= {"/signin"}/>
-// }
-
 import React, { useContext } from "react";
 import { Navigate } from "react-router-dom";
 import { authContext } from "../contexts/authContext";
