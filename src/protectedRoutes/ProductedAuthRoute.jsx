0b29c067fa396c2a8cb9a105fe236700fcb4cac6
abanoubsamir <abanoubmalek74@gmail.com>
2026-03-02 23:55:06 +0200

    update

--- src/protectedRoutes/ProductedAuthRoute.jsx
+++ src/protectedRoutes/ProductedAuthRoute.jsx
@@ -1,11 +1,3 @@
-// import React, { useContext } from "react";
-
-// export default function ProductedAuthRoute({ children }) {
-//   const { userToken } = useContext(authContext);
-//   const isLoggedIn = !!userToken;
-//   return isLoggedIn ? children : <Navigate to={"/"} />;
-// }
-
 import React, { useContext } from "react";
 import { Navigate } from "react-router-dom";
 import { authContext } from "../contexts/authContext";
